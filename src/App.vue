f6b7b4182b26464587a57735f86d6cd36c33ec75
salemdaniel007 <salemdaniel007@gmail.comm>
2021-07-05 23:00:28 +0100

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="flex-container">
+      <div class="container">
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
+      <router-view />
     </div>
-    <router-view/>
+    </div>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background-color: #cdd8e4;
+  background-image: linear-gradient(315deg, #9fbfe0 0%, #d7deec 74%);
+  background-attachment: fixed;
+  background-size: cover;
 }
 
-#nav {
-  padding: 30px;
+.flex-container {
+  display: flex;
+  height: 100vh;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container {
+  margin: auto;
+  padding: 2px 16px;
+  width: 80%;
+  height: 75vh;
+  border-radius: 2rem;
+  background-color: #f0f1f1;
+  backdrop-filter: blur(5px);
+  box-shadow: 0 32px 32px 0 rgba(0, 0, 0, 0.1), 200px 50px 100px 50px rgba(0, 0, 0, 0.2);
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="flex-container">
+      <div class="text">This is the text we are talking about here</div>
+      <div class="qr-code-container"></div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
   },
 };
 </script>
+
+<style>
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.qr-code-container {
+  border-radius: 2rem;
+  padding: 2px 16px;
+  width: 30%;
+  height: 70vh;
+  background-color: #010847;
+}
+
+</style>
